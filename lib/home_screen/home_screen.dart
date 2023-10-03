ad52d5832ebc3952e811b044521e642a241da819
AtefMMO <mohamedatefimb@gmail.com>
2023-10-03 12:25:40 +0300

    floating action button shows modalBottomSheet

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/app_theme.dart';
+import 'package:todoapp/home_screen/add_task_screen.dart';
+import 'package:todoapp/list_tap/list_screen.dart';
+import 'package:todoapp/settings_tap/settings_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String RouteName = 'HomeScreen';
@@ -11,6 +14,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   int selectedIndex = 0;
+  List<Widget> tapsList=[ListScreen(),SettingsScreen()];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('To DO List')),
@@ -31,7 +35,9 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+ShowAddTaskBottomSheet();
+        },
         child: Icon(
           Icons.add,
           size: 30,
@@ -39,6 +45,10 @@ class _HomeScreenState extends State<HomeScreen> {
         shape: StadiumBorder(side: BorderSide(color: Colors.white, width: 4)),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: tapsList[selectedIndex],
     );
+  }
+ShowAddTaskBottomSheet(){
+  showModalBottomSheet(context: context, builder: (context)=>AddTaskScreen())  ;
   }
 }
